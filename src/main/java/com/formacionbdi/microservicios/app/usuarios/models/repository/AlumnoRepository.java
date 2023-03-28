c68f277f8ec27453a86452cc2d9b967698ed6613
arochaj2 <arochaj2@gmail.com>
2023-03-28 22:15:09 +0200

    cambiando a postgres

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -10,7 +10,10 @@ import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 
 public interface AlumnoRepository extends PagingAndSortingRepository<Alumno, Long> {
 	
-	@Query("select a from Alumno a where a.nombre like %?1% or a.apellido like %?1%")
+	/*el upper y el concat se usa para que en la
+	 * busqueda omita mayusculas y minusculas
+	 * */
+	@Query("select a from Alumno a where upper(a.nombre) like upper(concat('%',?1,'%')) or upper(a.apellido) like upper(concat('%',?1,'%'))")
 	public List<Alumno> findByNombreOrApellido(String term);
 	
 	
--- src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlummnoServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlummnoServiceImpl.java
@@ -4,10 +4,11 @@ package com.formacionbdi.microservicios.app.usuarios.services;
 
 import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formacionbdi.microservicios.app.usuarios.client.CursoFeignClient;
 import com.formacionbdi.microservicios.app.usuarios.models.repository.AlumnoRepository;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
@@ -17,12 +18,42 @@ import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
 @Service
 public class AlummnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
 
+	@Autowired
+	private CursoFeignClient clienteCurso;
+	
+	
 	@Override
 	@Transactional(readOnly = true)
 	public List<Alumno> findByNombreOrApellido(String term) {
 
 		return repository.findByNombreOrApellido(term);
 	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Alumno> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return repository.findAllById(ids);
+	}
+
+	@Override
+	public void eliminarCursoAlumnoPorId(Long id) {
+		clienteCurso.eliminarCursoAlumnoPorId(id);
+		
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		
+		super.deleteById(id);
+		this.eliminarCursoAlumnoPorId(id);
+		
+		
+		
+	}
+	
+	
 
 		
 	
